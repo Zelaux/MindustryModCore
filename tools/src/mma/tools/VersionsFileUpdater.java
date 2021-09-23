4de8442ab9bb6a8f6314d59c1acf8a96a1612605
Zelaux <ura20060605@gmail.com>
2021-09-23 18:15:48 +0300

    github test10

--- tools/src/mma/tools/VersionsFileUpdater.java
+++ tools/src/mma/tools/VersionsFileUpdater.java
@@ -2,7 +2,9 @@ package mma.tools;
 
 import arc.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 public class VersionsFileUpdater {
@@ -10,13 +12,15 @@ public class VersionsFileUpdater {
         try {
             Process proc = Runtime.getRuntime().exec("git log --pretty=format:\"%H:%s\" -1");
             proc.waitFor();
-            String result = new String(proc.getInputStream().readAllBytes());
-            Log.info("result :@",result);
+
+//            String result = new String(.readNBytes(Integer.MAX_VALUE));
+            String result = new BufferedReader(new InputStreamReader(proc.getInputStream())).readLine();
+            Log.info("result :@", result);
 //            proc.destroy();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        arc.files.Fi.get("versions").walk(fi->{
+        arc.files.Fi.get("versions").walk(fi -> {
             try {
                 new URL("https://jitpack.io/com/github/Zelaux/ZelauxModCore/" + fi.readString() + "/build.log").openStream();
             } catch (IOException exception) {
