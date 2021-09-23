a3fe11579b424f1c3c92ab136e55121af9c284e5
Zelaux <ura20060605@gmail.com>
2021-09-23 17:56:29 +0300

    github test6

--- tools/src/mma/tools/VersionsFileUpdater.java
+++ tools/src/mma/tools/VersionsFileUpdater.java
@@ -8,7 +8,6 @@ import java.net.URL;
 public class VersionsFileUpdater {
     public static void main(String[] args) {
         try {
-            //здесь "sleep 15" и есть ваша консольная команда
             Process proc = Runtime.getRuntime().exec("git log --pretty=format:\"%H:%s\" -1");
             proc.waitFor();
             String result = new String(proc.getInputStream().readAllBytes());
