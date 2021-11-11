088133f7c041e293eb6b17b7adf68c01cf24816b
Zelaux <ura20060605@gmail.com>
2021-11-11 21:08:31 +0300

    Added parameters clearing in EventSender and EventReceiver

--- plugins/src/mma/tools/EventReceiver.java
+++ plugins/src/mma/tools/EventReceiver.java
@@ -1,64 +1,69 @@
 package mma.tools;
 
-import arc.Events;
-import arc.func.Cons;
-import arc.struct.ObjectMap;
+import arc.*;
+import arc.func.*;
+import arc.struct.*;
 
-public class EventReceiver {
-public     final String commandName;
-    private Object[] objects = null;
-    private ObjectMap<String, Object> parametersMap = new ObjectMap<>();
-public EventReceiver(EventSender sender){
-    this(sender.commandName);
-}
-    public EventReceiver(String commandName) {
+public class EventReceiver{
+    public final String commandName;
+    private final ObjectMap<String, Object> parametersMap = new ObjectMap<>();
+
+    public EventReceiver(EventSender sender){
+        this(sender.commandName);
+    }
+
+    public EventReceiver(String commandName){
         this.commandName = commandName;
     }
 
-    public boolean valid(Object[] objects) {
-        if (commandName.equals(objects[0]) && (objects.length - 1) % 2 == 0) {
-            try {
+    public boolean valid(Object[] objects){
+        parametersMap.clear();
+        if(commandName.equals(objects[0]) && (objects.length - 1) % 2 == 0){
+            try{
 
                 boolean valid = true;
-                for (int i = 1; i < objects.length - 1 && valid; i += 2) {
+                for(int i = 1; i < objects.length - 1 && valid; i += 2){
                     valid &= objects[i] instanceof String;
                     valid &= objects.length - 1 > i;
                 }
-                if (!valid) {
+                if(!valid){
                     throw new UnknownObjects();
                 }
                 parametersMap.clear();
-                for (int i = 1; i < objects.length; i += 2) {
-                    parametersMap.put((String) objects[i], objects[i + 1]);
+                for(int i = 1; i < objects.length; i += 2){
+                    parametersMap.put((String)objects[i], objects[i + 1]);
                 }
-                this.objects = objects;
                 return true;
-            } catch (UnknownObjects e) {
+            }catch(UnknownObjects e){
 
             }
         }
         return false;
     }
 
-    public boolean hasParameter(String name, Class<?> type) {
-        if (!parametersMap.containsKey(name))return false;
+    public boolean hasParameter(String name, Class<?> type){
+        if(!parametersMap.containsKey(name)) return false;
         Object object = parametersMap.get(name);
-        return  (type == null  || type.isInstance(object));
+        return (type == null || type.isInstance(object));
     }
 
-    public <T> T getParameter(String name) {
-        return (T) parametersMap.getNull(name);
+    public <T> T getParameter(String name){
+        return (T)parametersMap.getNull(name);
     }
 
-    public void post(Cons<EventReceiver> cons) {
+    public void post(Cons<EventReceiver> cons){
         Events.on(Object[].class, objects1 -> {
-            if (valid(objects1)) {
-                cons.get(this);
+            if(valid(objects1)){
+                try{
+                    cons.get(this);
+                }catch(Exception exception){
+                    exception.printStackTrace();
+                }
             }
         });
     }
 
-    private static class UnknownObjects extends Exception {
+    private static class UnknownObjects extends Exception{
 
     }
 }
--- plugins/src/mma/tools/EventSender.java
+++ plugins/src/mma/tools/EventSender.java
@@ -1,30 +1,41 @@
 package mma.tools;
 
-import arc.Events;
-import arc.struct.ObjectMap;
-import arc.struct.Seq;
+import arc.*;
+import arc.struct.*;
 
-public class EventSender {
-    public   final String commandName;
-    private ObjectMap<String,Object> parametersMap = new ObjectMap<>();
+public class EventSender{
+    public final String commandName;
+    private final ObjectMap<String, Object> parametersMap = new ObjectMap<>();
 
-    public EventSender(String commandName) {
-        this.commandName = commandName;
-    }
-    public EventSender(EventReceiver receiver) {
+    public EventSender(EventReceiver receiver){
         this(receiver.commandName);
     }
 
-    public <T> void setParameter(String name, T parameter) {
+    public EventSender(String commandName){
+        this.commandName = commandName;
+    }
+
+    public <T> void setParameter(String name, T parameter){
         parametersMap.put(name, parameter);
     }
 
-    public void fire() {
+    public void clear(){
+        parametersMap.clear();
+    }
+
+    public void fire(){
+        fire(false);
+    }
+
+    public void fire(boolean clear){
         Seq<Object> objects = new Seq<>();
-        objects.add( commandName);
-        for (ObjectMap.Entry<String, Object> entry : parametersMap) {
-            objects.add(entry.key,entry.value);
+        objects.add(commandName);
+        for(ObjectMap.Entry<String, Object> entry : parametersMap){
+            objects.add(entry.key, entry.value);
         }
         Events.fire(objects.toArray(Object.class));
+        if(clear){
+            clear();
+        }
     }
 }
