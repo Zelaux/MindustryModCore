3df18e35b429707ea59a7f583a40eb7c74cf9183
Zelaux <ura20060605@gmail.com>
2021-12-05 15:17:28 +0300

    Updated draw functions

--- core/src/mma/graphics/ModFill.java
+++ core/src/mma/graphics/ModFill.java
@@ -1,13 +1,11 @@
 package mma.graphics;
 
-import arc.func.Cons;
-import arc.func.Cons2;
-import arc.func.Floatc2;
-import arc.graphics.g2d.Fill;
-import arc.math.geom.Vec2;
-import arc.struct.FloatSeq;
+import arc.func.*;
+import arc.graphics.g2d.*;
+import arc.math.geom.*;
+import arc.struct.*;
 
-public class ModFill extends Fill {
+public class ModFill extends Fill{
     private static Vec2 vector = new Vec2();
     private static Vec2 u = new Vec2();
     private static Vec2 v = new Vec2();
@@ -18,9 +16,17 @@ public class ModFill extends Fill {
     private static boolean building;
     private static float circlePrecision = 0.4F;
 
-    public static void swirl(float x, float y, float radius, float finion, float angle) {
+    /**
+     * @deprecated use pie
+     */
+    @Deprecated
+    public static void swirl(float x, float y, float radius, float finion, float angle){
+        pie(x, y, radius, finion, angle);
+    }
+
+    public static void pie(float x, float y, float radius, float finion, float angle){
         final float sides = 50;
-        int max = (int) (sides * (finion + 0.001F));
+        int max = (int)(sides * (finion + 0.001F));
         vector.set(0.0F, 0.0F);
         floats.clear();
 
@@ -30,14 +36,14 @@ public class ModFill extends Fill {
             floats.add(vector.x + x, vector.y + y);
         };
         int startI = 0;
-        if (max % 2 != 0) {
+        if(max % 2 != 0){
             startI = 1;
             cons.get(0f);
             cons.get(1f);
             cons.get(1f);
 //            cons.get(2f);
         }
-        for (float i = startI; i < (max); i += 2f) {
+        for(float i = startI; i < (max); i += 2f){
             cons.get(i);
             cons.get(i + 1f);
             cons.get(i + 2f);
@@ -45,18 +51,23 @@ public class ModFill extends Fill {
         poly(floats);
     }
 
-    public static void circleRect(float x, float y, float radius) {
+    @Deprecated
+    public static void circleRect(float x, float y, float radius){
+        polyCircle(x, y, radius);
+    }
+
+    public static void polyCircle(float x, float y, float radius){
         swirl(x, y, radius, 1f, 0f);
     }
 
-    public static void spikesSwirl(float x, float y, float radius1, float length, float finion, float angle) {
-        spikesSwirl(x, y, radius1, length, finion, angle, 0.5f);
+    public static void spikedDonut(float x, float y, float radius1, float length, float finion, float angle){
+        spikedDonut(x, y, radius1, length, finion, angle, 0.5f);
     }
 
-    public static void spikesSwirl(float x, float y, float radius1, float length, float finion, float angle, float spikeOffset) {
+    public static void spikedDonut(float x, float y, float radius1, float length, float finion, float angle, float spikeOffset){
         final float sides = 50;
         final float radius2 = radius1 + length;
-        int max = (int) (sides * (finion + 0.001F));
+        int max = (int)(sides * (finion + 0.001F));
         vector.set(0.0F, 0.0F);
         floats.clear();
         Cons2<Float, Float> point = (i, radius) -> {
@@ -75,17 +86,25 @@ public class ModFill extends Fill {
             flush.run();
         };
         int startI = 0;
-        if (max % 2 != 0) {
+        if(max % 2 != 0){
             startI = 1;
             spike.get(0f);
         }
 
-        for (float i = startI; i < max; i++) {
+        for(float i = startI; i < max; i++){
             spike.get(i);
         }
     }
 
-    public static void doubleSwirl(float x, float y, float radius1, float radius2, float finion, float angle) {
+    /**
+     * @deprecated use donut
+     */
+    @Deprecated
+    public static void doubleSwirl(float x, float y, float radius1, float radius2, float finion, float angle){
+        donut(x, y, radius1, radius2, finion, angle);
+    }
+
+    public static void donut(float x, float y, float innerRadius, float outerRadius, float finion, float angle){
         float sides = 50.0F;
         float max = 50.0F;
         vector.set(0.0F, 0.0F);
@@ -93,17 +112,17 @@ public class ModFill extends Fill {
         Cons<Float> cons = (ix) -> {
             float v = 7.2F * ix;
             v = 360.0F * finion / 50.0F * ix;
-            vector.set(radius1, 0.0F).setAngle(v + angle);
+            vector.set(innerRadius, 0.0F).setAngle(v + angle);
             floats.add(vector.x + x, vector.y + y);
-            vector.set(radius2, 0.0F).setAngle(v + angle);
+            vector.set(outerRadius, 0.0F).setAngle(v + angle);
             floats.add(vector.x + x, vector.y + y);
         };
         Cons<Float> undoCons = (ix) -> {
             float v = 7.2F * ix;
             v = 360.0F * finion / 50.0F * ix;
-            vector.set(radius2, 0.0F).setAngle(v + angle);
+            vector.set(outerRadius, 0.0F).setAngle(v + angle);
             floats.add(vector.x + x, vector.y + y);
-            vector.set(radius1, 0.0F).setAngle(v + angle);
+            vector.set(innerRadius, 0.0F).setAngle(v + angle);
             floats.add(vector.x + x, vector.y + y);
         };
         Runnable flush = () -> {
@@ -111,7 +130,7 @@ public class ModFill extends Fill {
             floats.clear();
         };
         int startI = 0;
-        if (max % 2.0F != 0.0F) {
+        if(max % 2.0F != 0.0F){
             startI = 1;
             cons.get(0.0F);
             undoCons.get(1.0F);
@@ -121,7 +140,7 @@ public class ModFill extends Fill {
             flush.run();
         }
 
-        for(float i = (float)startI; i < max; i += 2.0F) {
+        for(float i = (float)startI; i < max; i += 2.0F){
             cons.get(i);
             undoCons.get(i + 1.0F);
             flush.run();
@@ -132,44 +151,46 @@ public class ModFill extends Fill {
 
     }
 
-    public static void tri(FloatSeq floats) {
-        if (floats.size < 6) return;
+    public static void tri(FloatSeq floats){
+        if(floats.size < 6) return;
         float[] items = floats.items;
         tri(items[0], items[1], items[2], items[3], items[4], items[5]);
     }
 
-    public static void quad(FloatSeq floats) {
-        if (floats.size < 8) return;
+    public static void quad(FloatSeq floats){
+        if(floats.size < 8) return;
         float[] items = floats.items;
         quad(items[0], items[1], items[2], items[3], items[4], items[5], items[6], items[7]);
     }
-    public static void crystalLine(float x, float y, float radius1, float radius2, float angle, int count,final float width) {
-        if (count==0)return;
+
+    public static void crystalLine(float x, float y, float radius1, float radius2, float angle, int count, final float width){
+        if(count == 0) return;
         final int sides = 50;
-        count=Math.min(sides,count);
+        count = Math.min(sides, count);
         float oneAngle = 360f / count;
-        Floatc2 point=(i, radius)->{
+        Floatc2 point = (i, radius) -> {
             vector.set(radius, 0.0F).setAngle(oneAngle * i + angle);
             floats.add(vector.x + x, vector.y + y);
         };
-        Floatc2 side=(i, i2)->{
+        Floatc2 side = (i, i2) -> {
             floats.clear();
-            point.get(i2,radius1);
-            point.get(i,radius2);
+            point.get(i2, radius1);
+            point.get(i, radius2);
 //            Vec2 cpy = vector.trns(oneAngle * i2 + angle, radius2-width, 0.0F).cpy();
 //            Vec2 cpy2 = vector.trns(oneAngle * i + angle, radius1-width, 0.0F).cpy();
-            point.get(i,radius2*((radius1-width)/radius1));
-            point.get(i2,radius1-width);
+            point.get(i, radius2 * ((radius1 - width) / radius1));
+            point.get(i2, radius1 - width);
             ModFill.quad(floats);
         };
         floats.clear();
-        for (float i = 0; i < count; i++) {
-            side.get(i,i-0.5f);
-            side.get(i,i+0.5f);
+        for(float i = 0; i < count; i++){
+            side.get(i, i - 0.5f);
+            side.get(i, i + 0.5f);
         }
     }
-    public static void crystal(float x, float y, float radius1, float radius2, float angle, int count) {
-        if (count == 0) return;
+
+    public static void crystal(float x, float y, float radius1, float radius2, float angle, int count){
+        if(count == 0) return;
         final float sides = 50;
         float oneAngle = 360f / count;
         float offset = oneAngle / 2f;
@@ -178,7 +199,7 @@ public class ModFill extends Fill {
             floats.add(vector.x + x, vector.y + y);
         };
         floats.clear();
-        for (float i = 0; i < count; i++) {
+        for(float i = 0; i < count; i++){
             floats.add(x, y);
             point.get(i - 0.5f, radius1);
             point.get(i, radius2);
--- core/src/mma/graphics/ModLines.java
+++ core/src/mma/graphics/ModLines.java
@@ -1,37 +1,39 @@
 package mma.graphics;
 
-import arc.func.Cons;
-import arc.graphics.g2d.Lines;
-import arc.math.geom.Rect;
-import arc.math.geom.Vec2;
-import arc.struct.FloatSeq;
+import arc.func.*;
+import arc.graphics.g2d.*;
+import arc.math.geom.*;
+import arc.struct.*;
 
 
-public class ModLines extends Lines {
+public class ModLines extends Lines{
     static FloatSeq floats = new FloatSeq();
     private static Vec2 vector = new Vec2();
 
-    public static void crystal(float x, float y, float radius1, float radius2, float angle, int count) {
-        if (count==0)return;
+    public static void crystal(float x, float y, float radius1, float radius2, float angle, int count){
+        if(count == 0) return;
         final float stroke = getStroke();
-        ModFill.crystalLine(x,y,radius1,radius2,angle,count,stroke);
-    } public static void swirl(float x, float y, float radius, float finion) {
+        ModFill.crystalLine(x, y, radius1, radius2, angle, count, stroke);
+    }
+
+    public static void swirl(float x, float y, float radius, float finion){
         swirl(x, y, radius, finion, 0.0F);
     }
 
-    public static void swirl(float x, float y, float radius, float finion, float angle) {
+    public static void swirl(float x, float y, float radius, float finion, float angle){
         float stroke = getStroke();
         float halfStroke = stroke / 2.0F;
-        ModFill.doubleSwirl(x, y, radius - halfStroke, radius + halfStroke, finion, angle);
+        ModFill.donut(x, y, radius - halfStroke, radius + halfStroke, finion, angle);
     }
 
-    public static void square(float x, float y, float rad) {
+    public static void square(float x, float y, float rad){
         rect(x - rad, y - rad, rad * 2.0F, rad * 2.0F);
     }
-    public static void rect(float x, float y, float width, float height, float offsetx, float offsety, float rot) {
+
+    public static void rect(float x, float y, float width, float height, float offsetx, float offsety, float rot){
         float stroke = getStroke();
-        Vec2[] points1 = ModGeometry.rectPoints(x+offsetx, y+offsety, width, height, rot);
-        Vec2[] points2 = ModGeometry.rectPoints(x + stroke+offsetx, y + stroke+offsety, width - stroke*2f, height - stroke*2f, rot);
+        Vec2[] points1 = ModGeometry.rectPoints(x + offsetx, y + offsety, width, height, rot);
+        Vec2[] points2 = ModGeometry.rectPoints(x + stroke + offsetx, y + stroke + offsety, width - stroke * 2f, height - stroke * 2f, rot);
         Cons<Integer> side = (index) -> {
             int index2 = (index + 1) % 4;
             floats.clear();
@@ -48,19 +50,19 @@ public class ModLines extends Lines {
         floats.clear();
     }
 
-    public static void rect(float x, float y, float width, float height) {
+    public static void rect(float x, float y, float width, float height){
         rect(x, y, width, height, 0);
     }
 
-    public static void rect(float x, float y, float width, float height, float rot) {
+    public static void rect(float x, float y, float width, float height, float rot){
         rect(x, y, width, height, 0, 0, rot);
     }
 
-    public static void rect(Rect rect) {
+    public static void rect(Rect rect){
         rect(rect.x, rect.y, rect.width, rect.height, 0);
     }
 
-    public static void rect(float x, float y, float width, float height, int offset) {
-        rect(x, y, width, height, (float) offset, (float) offset, 0F);
+    public static void rect(float x, float y, float width, float height, int offset){
+        rect(x, y, width, height, (float)offset, (float)offset, 0F);
     }
 }
